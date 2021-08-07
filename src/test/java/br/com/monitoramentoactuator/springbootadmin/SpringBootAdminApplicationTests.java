1ad4be7cd15165822fc976a7f9402c1b6ece5da7
Adriano Gonçalves de Souza <adriangonc@yahoo.com.br>
2021-08-07 18:52:59 -0300

    First commit

---
+++ src/test/java/br/com/monitoramentoactuator/springbootadmin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,9 @@
+package br.com.monitoramentoactuator.springbootadmin;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminApplicationTests {
+
+}
